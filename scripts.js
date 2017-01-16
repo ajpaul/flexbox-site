2e0b61bc8655daa211f3671aff2f18fcb309cf1a
Adam Paul <ajp01@acuitysso.com>
2017-01-16 10:22:21 -0500

    Finished flex animation

--- scripts.js
+++ scripts.js
@@ -1,14 +1,21 @@
 var panels = document.querySelectorAll('.panel');
 
-
 for(var i = 0; i < panels.length; i++) {
-    panels[i].addEventListener("click", function(e) {
-        //collapse all other panels to flex 1
-        var parent = e.srcElement.parentElement.classList[1];
-        var panel = document.querySelector(`.${parent}`);
-        panel.style.flex = 2;
-    });
+    panels[i].addEventListener("click", panelClick);
 }
 
-//slide text in and out
-//<p> 1 and 3 should not be visible until the panel is clicked
+//collapse all other panels to flex 1
+function panelClick(e) {
+    
+    var parent = e.srcElement.parentElement.classList[1];
+    var panel = document.querySelector(`.${parent}`);
+    var node = e.srcElement.parentNode.parentElement.firstElementChild;
+
+
+    while( node && node.nodeType === 1) {
+        node.style.flex = 1;
+        node = node.nextElementSibling;
+    }
+
+    panel.style.flex = 2; //set clicked panel to flex 2
+}
