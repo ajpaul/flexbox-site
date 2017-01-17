287a8b1d2ca899b3bd142087d4d0100f9f3d971f
Adam Paul <ajp01@acuitysso.com>
2017-01-17 10:20:24 -0500

    Finished JS. I am an idiot

--- scripts.js
+++ scripts.js
@@ -1,21 +1,9 @@
 var panels = document.querySelectorAll('.panel');
-
-for(var i = 0; i < panels.length; i++) {
-    panels[i].addEventListener("click", panelClick);
-}
+panels.forEach(panel => panel.addEventListener("click", panelClick));
 
 //collapse all other panels to flex 1
 function panelClick(e) {
-    
-    var parent = e.srcElement.parentElement.classList[1];
-    var panel = document.querySelector(`.${parent}`);
-    var node = e.srcElement.parentNode.parentElement.firstElementChild;
 
-
-    while( node && node.nodeType === 1) {
-        node.style.flex = 1;
-        node = node.nextElementSibling;
-    }
-
-    panel.style.flex = 2; //set clicked panel to flex 2
+    this.classList.add("open");
+    panels.forEach(panel => { if(panel !== this) panel.classList.remove("open") })
 }
